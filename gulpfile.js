ef35e05138233e160709bceaae94971bba812bb8
Joao - Mac <j@guiatech.com.br>
2015-01-27 00:32:38 -0200

    trying cssmin to make it completed at all

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,31 @@
 var gulp = require( 'gulp' ) ,
 	/*path = require( 'path' ),*/
 	rename = require( 'gulp-rename' ),
-	uglify = require( 'gulp-uglify' ),
-	compass = require( 'gulp-compass' ) ;
+  uglify = require( 'gulp-uglify' ),
+	compass = require( 'gulp-compass' )
+  cssmin = require( 'gulp-cssmin' ) ;
 
-gulp.task( 'uglify', function() {
-  gulp.src( './static/js/common.js' )
-        .pipe( uglify() )
-		.pipe( rename( 'common.min.js' ) )
-        .pipe( gulp.dest( './static/js/' ) ) ;
-} ) ;
 
 gulp.task( 'compass' , function() {
-  gulp.src( './static/css/*.scss' )
+  gulp.src( './static/css/base.scss' )
     .pipe( compass( {
       config_file: './config.rb',
       css: 'static/css',
       sass: 'static/css'
     } ) )
-    .pipe( gulp.dest( './static/css/' ) );
+    .pipe( cssmin() )
+    .pipe( rename( 'base.min.css' ) )
+    .pipe( gulp.dest( './static/css/' ) ) ;
 });
+
+gulp.task( 'uglify', function() {
+  gulp.src( './static/js/common.js' )
+    .pipe( uglify() )
+		.pipe( rename( 'common.min.js' ) )
+    .pipe( gulp.dest( './static/js/' ) ) ;
+} ) ;
 
 gulp.task( 'watch', function() {
 	gulp.watch( './static/js/common.js', ['uglify'] ) ;
-	gulp.watch( './static/css/base.scss', ['compass'] ) ;
+  gulp.watch( './static/css/base.scss', ['compass'] ) ;
 } ) ;

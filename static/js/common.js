21f37253ca3f9ba2e176ae86e4ab5affb9ddcabb
Joao - Mac <j@guiatech.com.br>
2015-01-30 03:06:33 -0200

    last pie charts

--- static/js/common.js
+++ static/js/common.js
@@ -41,25 +41,69 @@ jobviteservers.controller( 'showListBuildFirewall' , [ '$scope', '$http', functi
 		}
 	}
 
+	$scope.$on('ngRepeatFinished', function(ngRepeatFinishedEvent) {
+		var name_pie = '.unit-pie' ;
+
+		//execute after ng-repeat finished function
+	    $( '.box-unit .chart .org-pie,
+	    	.box-functional .chart .org-pie', servers.configs.list_servers ).each( function( i ) {
+	    	var unit_chart = $( this ),
+	    	ctx = unit_chart.get( 0 ).getContext( "2d" ),
+	    	code_covered_v = parseInt( unit_chart.data( 'code-covered' ) ),
+	    	code_not_covered_v = parseInt( unit_chart.data( 'code-not-covered' ) ),
+			data = [
+				{
+					value: code_covered_v,
+					color: "#70AD47",
+					highlight: "#70AD47",
+					label: "value"
+				},
+				{
+					value: code_not_covered_v,
+					color: "#EC7C30",
+					highlight: "#EC7C30",
+					label: "value"
+				}
+			]
+
+			var unit_chart_mount = new Chart(ctx).Pie( data, servers.configs.options ) ;
+
+	    } ) ;
+	});
+
+} ] ).directive( 'onFinishRender', [ '$timeout', function ( $timeout ) {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attr) {
+            if (scope.$last === true) {
+                $timeout(function () {
+                    scope.$emit('ngRepeatFinished');
+                } ) ;
+            }
+        }
+    }
 } ] ) ;
 /*/ANGULAR FUNCTIONS*/
 
 
 //server configs
 servers.configs = {
-
 	container: '.container',
-
 	list_servers: '.list-servers',
-
-	init: function() {
-		var that = this, container = $( that.container ) ;
-	}
+	options: {
+		scaleShowLabelBackdrop: true,
+		scaleBackdropColor: "rgba(255,255,255,0.75)",
+		scaleBeginAtZero: true,
+		scaleBackdropPaddingY: 2,
+		scaleBackdropPaddingX: 2,
+		scaleShowLine: true,
+		segmentShowStroke: true,
+		segmentStrokeColor: "#fff",
+		segmentStrokeWidth: 1,
+		animationSteps: 100,
+		animationEasing: "easeOutBounce",
+		animateRotate: true,
+		animateScale: false,
+		legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>"
+    }
 } ;
-
-
-
-//ready function
-$( function(){
-	//servers.configs.init() ;
-}) ;
